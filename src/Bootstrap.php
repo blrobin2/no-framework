efd0f2b81787743a66ab8b6df334f1d2a3abf3de
BT Robinson <brobinson88@gmail.com>
2015-06-29 11:29:09 -0400

    Introduce HTTP handling with Symfony's HTTP Foundation

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -2,6 +2,11 @@
 
 require __DIR__.'/../vendor/autoload.php';
 
+use Symfony\Component\HttpFoundation\Response;
+use Whoops\Run;
+use Whoops\Handler\PrettyPageHandler;
+use Symfony\Component\HttpFoundation\Request;
+
 error_reporting(E_ALL);
 
 $environment = 'development';
@@ -9,9 +14,9 @@ $environment = 'development';
 /**
  * Register the error handler
  */
-$whoops = new \Whoops\Run;
+$whoops = new Run;
 if ($environment !== 'production') {
-    $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler());
+    $whoops->pushHandler(new PrettyPageHandler());
 } else {
     $whoops->pushHandler(function($e) {
         echo 'Friendly error page and send an email to the developer';
@@ -19,4 +24,20 @@ if ($environment !== 'production') {
 }
 $whoops->register();
 
-throw new \Exception;
+$request = Request::createFromGlobals();
+$response = new Response(
+    'Content',
+    Response::HTTP_OK,
+    array('content-type' => 'text/html')
+);
+
+// Sample success
+//$content = '<h1>Hello World</h1>';
+//$response->setContent($content);
+
+// Sample 404
+//$response->setContent('404 - Page not found');
+//$response->setStatusCode(404);
+
+$response->prepare($request);
+$response->send();
