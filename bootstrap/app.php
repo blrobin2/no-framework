1f2a8ccad975279ffe1f122cb97b8be3617de649
BT Robinson <brobinson88@gmail.com>
2015-06-30 09:44:32 -0400

    reorganize some of the files, putting the application bindings in a bootstrap folder.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,6 @@
 <?php namespace Framework;
 
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 use Dotenv\Dotenv;
 use FastRoute\Dispatcher;
@@ -10,9 +10,15 @@ use Whoops\Handler\PrettyPageHandler;
 use Symfony\Component\HttpFoundation\Request;
 use FastRoute\RouteCollector;
 
-$dotenv = new Dotenv(__DIR__.'/../');
+/**
+ * Load the environment variables
+ */
+$dotenv = new Dotenv(__DIR__ . '/../');
 $dotenv->load();
 
+/**
+ * Set the degree of error reporting
+ */
 error_reporting(getenv('ERROR_REPORTING'));
 $environment = getenv('ENVIRONMENT');
 
@@ -23,26 +29,40 @@ $whoops = new Run;
 if ($environment !== 'production') {
     $whoops->pushHandler(new PrettyPageHandler());
 } else {
-    $whoops->pushHandler(function($e) {
+    $whoops->pushHandler(function ($e) {
         echo 'Friendly error page and send an email to the developer';
     });
 }
 $whoops->register();
 
-$injector = include 'Dependencies.php';
+/**
+ * Pull in the DIC
+ */
+$injector = include 'dependencies.php';
 
 $request = $injector->make('Framework\Http\Request');
 $response = $injector->make('Framework\Http\Response');
 
+/**
+ * Pull in the defined routes.
+ *
+ * @param RouteCollector $r
+ */
 $routeDefinitionCallback = function (RouteCollector $r) {
-    $routes = include('Routes.php');
+    $routes = include __DIR__.'/../src/routes.php';
     foreach ($routes as $route) {
         $r->addRoute($route[0], $route[1], $route[2]);
     }
 };
 
+/**
+ * Load the routes into the dispatcher.
+ */
 $dispatcher = \FastRoute\simpleDispatcher($routeDefinitionCallback);
 
+/**
+ * Dispatch the routes, setting the response accordingly.
+ */
 $routeInfo = $dispatcher->dispatch($request->getMethod(), $request->getPathInfo());
 switch ($routeInfo[0]) {
     case Dispatcher::NOT_FOUND:
@@ -63,6 +83,8 @@ switch ($routeInfo[0]) {
         break;
 }
 
-// Prepare and send the response
+/**
+ * Prepare and send the response.
+ */
 $response->prepare($request);
 $response->send();
--- bootstrap/dependencies.php
+++ bootstrap/dependencies.php
@@ -4,25 +4,25 @@ $injector = new Auryn\Injector;
 
 $injector->share('Framework\Http\Request');
 $injector->define('Framework\Http\Request', [
-    ':query' => $_GET,
+    ':query'   => $_GET,
     ':request' => $_POST,
-    '' => array(),
-    ':server' => $_SERVER,
-    ':files' => $_FILES,
+    ''         => array(),
+    ':server'  => $_SERVER,
+    ':files'   => $_FILES,
     ':cookies' => $_COOKIE,
 ]);
 
 $injector->share('Framework\Http\Response');
 
 $injector->define('League\Plates\Engine', [
-    ':directory' => __DIR__.'/../templates',
+    ':directory' => __DIR__ . '/../templates',
 ]);
 
 $injector->alias('Framework\Templates\Renderer', 'Framework\Templates\PlatesRenderer');
 $injector->share('Framework\Templates\PlatesRenderer');
 
 $injector->define('Framework\Page\FilePageReader', [
-    ':pageFolder' => __DIR__.'/../pages',
+    ':pageFolder' => __DIR__ . '/../pages',
 ]);
 $injector->alias('Framework\Page\PageReader', 'Framework\Page\FilePageReader');
 $injector->share('Framework\Page\FilePageReader');
